cade005661260d68d473ab70ab84dca4154f82ed
Tim Doyle <tim.doyle@hotmail.com>
2021-08-20 13:54:44 +0930

    Update chart.js

--- static/script/chart.js
+++ static/script/chart.js
@@ -4,7 +4,7 @@ google.charts.setOnLoadCallback(drawChart);
 function drawChart() {
     var json = $.ajax({
         url: "/gauge",
-        dataType:"json",
+        //dataType:"json",
         async: false
     }).responseText;
 
