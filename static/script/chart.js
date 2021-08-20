d1064fdd1a4ad8a1746f489a80bce52bb07c18a0
Tim Doyle <tim.doyle@hotmail.com>
2021-08-20 21:09:31 +0930

    Update chart.js

--- static/script/chart.js
+++ static/script/chart.js
@@ -1,27 +1,38 @@
-function drawChart() {
-    var options = {
-        width: 480, height: 480,
-        greenColor: '#5cb85c', greenFrom: 0, greenTo: 29,
-        yellowColor: '#f0ad4e', yellowFrom: 40, yellowTo: 49,
-        redColor: '#d9534f', redFrom: 50, redTo: 60,
-        max: 60,
-        minorTicks: 5
-    };
-    $.ajax({
-        url: "/gauge",
-        dataType: "json"
-    }).done(function(data) {
-        var arr = [["KPh"]];
-        $.each(data.kph, function() {
-            var kph = [this.kph];
-            arr.push(kph);
-        });
-        var data = google.visualization.arrayToDataTable(arr);
-        var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
-        chart.draw(data, options);
-        //$("h3").append(" Sorted by  "+dimension);
-    });
-}
 google.charts.load('current', {'packages':['gauge']});
 google.charts.setOnLoadCallback(drawChart);
 
+function drawChart() {
+
+  var data = google.visualization.arrayToDataTable([
+    ['Label', 'Value'],
+    ['Memory', 80],
+    ['CPU', 55],
+    ['Network', 68]
+  ]);
+
+  var options = {
+  width: 480, height: 480,
+  greenColor: '#5cb85c', greenFrom: 0, greenTo: 29,
+  yellowColor: '#f0ad4e', yellowFrom: 40, yellowTo: 49,
+  redColor: '#d9534f', redFrom: 50, redTo: 60,
+  max: 60,
+  minorTicks: 5
+  };
+
+  var chart = new google.visualization.Gauge(document.getElementById('chart_div'));
+
+  chart.draw(data, options);
+
+  setInterval(function() {
+    //data.setValue(0, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 13000);
+  setInterval(function() {
+    //data.setValue(1, 1, 40 + Math.round(60 * Math.random()));
+    chart.draw(data, options);
+  }, 5000);
+  setInterval(function() {
+    //data.setValue(2, 1, 60 + Math.round(20 * Math.random()));
+    chart.draw(data, options);
+  }, 26000);
+}
