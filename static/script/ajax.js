9c120e8f4c7fe996f6b40fc8a59d3e5cd213bd09
Tim Doyle <tim.doyle@hotmail.com>
2021-08-20 16:43:20 +0930

    Update ajax.js

--- static/script/ajax.js
+++ static/script/ajax.js
@@ -1,11 +1,11 @@
 $('#a1').click(function() {
     $.ajax({
         url: '/gauge',
-        type: 'GET',
-        success: function(result) { //we got the response
-            alert('Successfully called');
-        },
-        error: function(req, err){ alert('Broken ' + err); }
-
+        type: 'GET'
     })
+    .done(function(data) {alert("Gzgz")})
+    .fail(function(xhr, status, error) {
+        var errorMessage = xhr.status + ': ' + xhr.statusText
+        alert('Error - ' + errorMessage);
+    }
 })
